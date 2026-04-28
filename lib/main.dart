897ab0d3100049c59ff8f46bf50f35a96c75a139
tekboxs <tekboxs@gmail.com>
2026-04-28 18:32:26 -0400

    fix: use path-based web routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:metrifica_landing/app/core/app_widget.dart';
 import 'package:metrifica_landing/app/core/config/app_config.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
 
   await Supabase.initialize(
     url: AppConfig.supabaseUrl,
